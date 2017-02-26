952154b3103c7f9724b488c6365266135e8829b5
benjer3 <benjer3@outlook.com>
2017-02-25 20:52:38 -0500

    Basic skeleton code

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour
+{
+
+    public int _maxHealth = 2;
+    public float _moveSpeed = 5f;
+
+
+    private int _health;
+
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+}
