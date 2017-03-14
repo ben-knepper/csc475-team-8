15c1aa616aa44dba5f5f4060c1baa0ccff1751de
jman809 <jts8506@uncw.edu>
2017-03-14 15:27:26 -0400

    Gun Fires Now

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -8,13 +8,85 @@ public class GunController : MonoBehaviour {
     public float weaponRange = 50f;                                     // Distance in Unity units over which the player can fire
     public float hitForce = 100f;                                       // Amount of force which will be added to objects with a rigidbody shot by the player
     public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
-                                                                        // Use this for initialization
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);    // WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
+    private AudioSource gunAudio;                                       // Reference to the audio source which will play our shooting sound effect
+    private LineRenderer laserLine;                                     // Reference to the LineRenderer component which will display our laserline
+    private float nextFire;												// Float to store the time the player will be allowed to fire again, after firing
+
+    // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Get and store a reference to our LineRenderer component
+        laserLine = GetComponent<LineRenderer>();
+
+        // Get and store a reference to our AudioSource component
+        gunAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Check if the player has pressed the fire button and if enough time has elapsed since they last fired
+
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            // Update the time when our player can fire next
+            nextFire = Time.time + fireRate;
+
+            // Start our ShotEffect coroutine to turn our laser line on and off
+            StartCoroutine(ShotEffect());
+            // Create a vector at the gun end
+            Vector3 rayOrigin = gunEnd.transform.position;
+            // Declare a raycast hit to store information about what our raycast has hit
+            RaycastHit hit;
+
+            // Set the start position for our visual effect for our laser to the position of gunEnd
+            laserLine.SetPosition(0, gunEnd.position);
+
+            // Check if our raycast has hit anything
+            if (Physics.Raycast(rayOrigin, gunEnd.transform.forward, out hit, weaponRange))
+            {
+                // Set the end position for our laser line 
+                laserLine.SetPosition(1, hit.point);
+
+                // Get a reference to a health script attached to the collider we hit
+                Drone health = hit.collider.GetComponent<Drone>();
+
+                // If there was a health script attached
+                if (health != null)
+                {
+                    // Call the damage function of that script, passing in our gunDamage variable
+                    //health.Damage(gunDamage);
+                }
+
+                // Check if the object we hit has a rigidbody attached
+                if (hit.rigidbody != null)
+                {
+                    // Add force to the rigidbody we hit, in the direction from which it was hit
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+            }
+            else
+            {
+                // If we did not hit anything, set the end of the line to a position directly in front of the gun end at the distance of weaponRange
+                laserLine.SetPosition(1, rayOrigin + (gunEnd.transform.forward * weaponRange));
+            }
+        }
+    }
+    private IEnumerator ShotEffect()
+    {
+        //Debug.Log("Firing");
+        // Play the shooting sound effect
+        gunAudio.Play();
+
+        // Turn on our line renderer
+        laserLine.enabled = true;
+
+        //Wait for .07 seconds
+        yield return shotDuration;
+
+        // Deactivate our line renderer after waiting
+        laserLine.enabled = false;
+    }
 }
