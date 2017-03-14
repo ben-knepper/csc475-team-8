90346c1e26d96c5479ec592f618f755dfe68de57
jman809 <jts8506@uncw.edu>
2017-03-14 14:41:15 -0400

    Begining Gun Controller

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public int gunDamage = 1;                                           // Set the number of hitpoints that this gun will take away from shot objects with a health script
+    public float fireRate = 0.25f;                                      // Number in seconds which controls how often the player can fire
+    public float weaponRange = 50f;                                     // Distance in Unity units over which the player can fire
+    public float hitForce = 100f;                                       // Amount of force which will be added to objects with a rigidbody shot by the player
+    public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
+                                                                        // Use this for initialization
+    void Start () {
 		
 	}
 	
