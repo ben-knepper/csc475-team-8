31021a832564bdd062e7556038b44736692e2b25
Cole <jrc9569@uncw.edu>
2017-04-04 15:28:11 -0400

    VR compatibility fixes

--- Assets/Scripts/LockerInteract.cs
+++ Assets/Scripts/LockerInteract.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class LockerInteract : MonoBehaviour {
 
 	public float interactDistance = 5f;
+    public GameObject hand;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
 		
 	}
@@ -15,8 +16,8 @@ public class LockerInteract : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.F) || Input.GetButtonDown("Fire2") || SixenseInput.Controllers[0].GetButton(SixenseButtons.TRIGGER)) {
-			Ray ray = new Ray (transform.position, transform.forward);
+		if (Input.GetKeyDown (KeyCode.F) || Input.GetButtonDown("Fire2") || SixenseInput.Controllers[0].GetButtonDown(SixenseButtons.TRIGGER)) {
+			Ray ray = new Ray (hand.transform.position, hand.transform.forward);
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit, interactDistance)) {
 				if (hit.collider.CompareTag ("Locker")) {
